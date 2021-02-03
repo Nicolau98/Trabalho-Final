dfa779a80f789c475f77925c1d00188456306c18
Nicolau Barranha <nicolaubarranha999@hotmail.com>
2021-02-03 22:32:24 +0000

    Listas

---
+++ Trabalho Final/Models/CurriculoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trabalho_Final.Models
+{
+    public class Curriculo
+    {
+        public List<Experiencia> Experiencias { get; set; }
+
+        public List<Educacao> Educacaos { get; set; }
+
+        public List<Lingua> Linguas { get; set; }
+
+        public List<CartaConducao> CartaConducaos { get; set; }
+    }
+}
