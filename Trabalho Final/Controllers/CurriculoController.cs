d1694c6003ecb31bf51aca445f9cd6ad46d70c84
Nicolau Barranha <nicolaubarranha999@hotmail.com>
2021-02-04 01:21:04 +0000

    Criar Apresentação

---
+++ Trabalho Final/Controllers/CurriculoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trabalho_Final.Models;
+
+namespace Trabalho_Final.Controllers
+{
+    public class CurriculoController : Controller
+    {
+        public List<Experiencia> Experiencia { get; internal set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Trabalho Final/Models/ApresentacaoModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trabalho_Final.Models
+{
+    public class Apresentacao
+    {
+        public int ApresentacaoId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [StringLength(10)]
+        public string Nascimento { get; set; }
+
+        [StringLength(115)]
+        public string Morada { get; set; }
+
+        [Required]
+        [Display(Name = "Pequeno texto")]
+        public string Texto { get; set; }
+
+        public byte[] Foto { get; set; }
+    }
+}
